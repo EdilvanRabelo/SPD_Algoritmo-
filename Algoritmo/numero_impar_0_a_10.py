8974f3fae2dda6024d7ed2c6b7d8ccbdfaa1b45a
Edilvan Rabelo <125370864+EdilvanRabelo@users.noreply.github.com>
2025-02-16 20:31:39 -0300

    praticando

--- Algoritmo/numero_impar_0_a_10.py
+++ Algoritmo/numero_impar_0_a_10.py
@@ -1,8 +1,25 @@
-# Criar um metido que mostre os numeros números ímpares de 0 a 10.
+# Criar um metodo que mostre se o numero é impar de 0 a 10.
+# Solicitar ao usuario um numero de 0 a 10.
+# Verificar se o numero é impar.   
+#  Se for, mostrar uma mensagem dizendo que o numero é impar. 
+# Se nao for, mostrar uma mensagem dizendo que o numero nao é impar. 
+def pedir_numero(minimo, maximo):
+    while True:
+        try:
+            numero = int(input(f"Digite um número entre {minimo} e {maximo}: "))
+            if minimo <= numero <= maximo:
+                return numero
+            else:
+                print("Número fora do intervalo. Tente novamente.")
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número.")
 
-# Percorre os números de 0 a 10
-for num in range(11):  
-    # Verifica se o número é ímpar (resto da divisão por 2 diferente de zero)
-    if num % 2 != 0:  
-        # Imprime o número ímpar
-        print(num)
+def numero_impar_0_a_10():
+    numero = pedir_numero(0, 10)
+
+    if numero % 2 != 0:
+        print(f"O número {numero} é ímpar.")
+    else:
+        print(f"O número {numero} não é ímpar.")
+
+numero_impar_0_a_10()
